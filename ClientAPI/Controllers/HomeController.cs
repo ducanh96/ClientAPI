cb07894a0a2a5b1631efb2eace756c5ec8283b45
TranHuong <tranhuong24081996@gmail.com>
2017-12-19 16:44:50 +0700

    ClientAPI\Content\Site.css

--- ClientAPI/Controllers/HomeController.cs
+++ ClientAPI/Controllers/HomeController.cs
@@ -48,5 +48,35 @@ namespace ClientAPI.Controllers
             return View();
 
         }
+        public ActionResult SXMN()
+        {
+            using (var cliet = new HttpClient())
+            {
+
+                cliet.BaseAddress = new Uri("http://localhost:50132/");
+
+                cliet.DefaultRequestHeaders.Accept.Clear();
+                cliet.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = cliet.GetAsync("lottezy/1");
+                response.Wait();
+
+                var result = response.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var KQ = result.Content.ReadAsAsync<ResponseLocationPrize>();
+
+                    KQ.Wait();
+                    ResponseLocationPrize res = KQ.Result;
+                    if (res.code == 0)
+                    {
+                        List<ViewModelLocationPrize> kq = res.data;
+                        return View(kq);
+                    }
+
+                }
+
+            }
+            return View();
+        }
     }
 }
